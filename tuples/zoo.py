73290bdd1cc76b2de479c0e8ba55ced0ac363953
Jessica Swift <jessica.nicole.swift@gmail.com>
2018-07-18 06:44:47 -0500

    finished zoo

--- tuples/zoo.py
+++ tuples/zoo.py
@@ -1,9 +1,33 @@
-zootuples = ('zebra', 'elephant', 'flamingo', 'camel', 'polar bear')
+# create a tuple named zoo
+zoo = ('zebra', 'elephant', 'flamingo', 'camel', 'polarbear')
 
-print(zootuples.index('flamingo'))
+# find animal using index
+print(zoo.index('flamingo'))
 
-for value in zootuples:
+# determine if animal is in tuple
+animal = []
 
-    if not zootuples.animal('flamingo'):
-        filtered_zootuples.append('flamingo')
-        print(filtered_zootuples)
+def find(animal):
+    for value in zoo:
+        if animal in zoo:
+            print(f'{animal} is currently at the Zoo.')
+        else:
+            print(f'{animal} is not at the Zoo.')
+
+find('flamingo')
+find('lion')
+
+# create variable for each animal
+(zebra, elephant, flamingo, camel, polarbear) = zoo
+print(elephant)
+
+# convert tuple to list
+zoo_list = list(zoo)
+print(zoo_list)
+
+# use extend to add 3 animals
+zoo_list.extend(['kitty', 'cardinal', 'dolphin'])
+print(zoo_list)
+
+# convert list back to tuple
+print(tuple(zoo_list))
